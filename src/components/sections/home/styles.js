7d93ddd2559f3bcceae7471259ae0c6c54dc40d7
marcusv77 <marcus.vsf@aluno.ufop.edu.br>
2021-01-27 15:14:11 -0300

    Update 27/01/2021

--- src/components/sections/home/styles.js
+++ src/components/sections/home/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import logo from '../../../assets/Home/logo.png';
+import logo from '../../../assets/home/logo.png';
 
 export const Logo = styled.img.attrs({
   src: logo,
